30d77188bb3e87a149fd6efbc1916eb8471ebf2b
Eugene Taran <yevgenii.taran@gmail.com>
2020-11-09 14:18:20 +0200

    init app

---
+++ src/main/java/com/etaran/tetris/WebsocketTetrisApplication.java
@@ -0,0 +1,13 @@
+package com.etaran.tetris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketTetrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketTetrisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etaran/tetris/WebsocketTetrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etaran.tetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTetrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
