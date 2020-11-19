e3fdef06f1942a0e8aa12e3af49b26e1c5ecb72f
Eugene Taran <yevgenii.taran@gmail.com>
2020-11-19 18:46:32 +0200

    add field id return

--- src/main/java/com/etaran/tetris/controller/FieldController.java
+++ src/main/java/com/etaran/tetris/controller/FieldController.java
@@ -1,28 +1,19 @@
 package com.etaran.tetris.controller;
 
 import com.etaran.tetris.controller.dto.FieldDto;
-import com.etaran.tetris.controller.dto.Message;
-import com.etaran.tetris.controller.dto.OutputMessage;
 import com.etaran.tetris.model.Field;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Controller
 public class FieldController {
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public OutputMessage greeting(Message message) throws Exception {
-//        Field field = new Field();
-//        FieldDto fieldDto = new FieldDto(field.getFieldId());
-//        Thread.sleep(1000); // simulated delay
-//        return fieldDto;
-        String time = new SimpleDateFormat("HH:mm").format(new Date());
-        return new OutputMessage(message.getFrom(), message.getText(), time);
+    @MessageMapping("/tetris")
+    @SendTo("/topic/game")
+    public FieldDto getField() {
+        Field field = new Field();
+        return new FieldDto(field.getFieldId());
     }
 
 }
