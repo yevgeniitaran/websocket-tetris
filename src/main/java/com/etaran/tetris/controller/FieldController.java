43a4435bb6931df864cba0991011473a7b680fd1
Eugene Taran <yevgenii.taran@gmail.com>
2020-11-22 23:16:02 +0200

    use field service

--- src/main/java/com/etaran/tetris/controller/FieldController.java
+++ src/main/java/com/etaran/tetris/controller/FieldController.java
@@ -2,6 +2,7 @@ package com.etaran.tetris.controller;
 
 import com.etaran.tetris.controller.dto.FieldDto;
 import com.etaran.tetris.model.Field;
+import com.etaran.tetris.service.FieldService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -9,11 +10,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class FieldController {
 
+    private final FieldService fieldService;
+
+    public FieldController(FieldService fieldService) {
+        this.fieldService = fieldService;
+    }
+
     @MessageMapping("/tetris")
     @SendTo("/topic/game")
     public FieldDto getField() {
-        Field field = new Field();
-        return new FieldDto(field.toString());
+        fieldService.tick();
+        return new FieldDto(fieldService.getField().toString());
     }
 
 }
