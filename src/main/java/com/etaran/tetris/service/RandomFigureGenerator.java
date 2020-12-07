8ea89df69e16b84f25cb2fee5235010b24e699ea
Eugene Taran <yevgenii.taran@gmail.com>
2020-12-07 23:29:06 +0200

    add random elements and color

--- src/main/java/com/etaran/tetris/service/RandomFigureGenerator.java
+++ src/main/java/com/etaran/tetris/service/RandomFigureGenerator.java
@@ -3,10 +3,14 @@ package com.etaran.tetris.service;
 import com.etaran.tetris.model.Figure;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class RandomFigureGenerator {
 
+    Random random = new Random();
+
     public Figure produceFigure() {
-        return Figure.ORANGE_L;
+        return Figure.values()[random.nextInt(7)];
     }
 }
